1f09156c9716fd4ab5b19fb04c4a11c0e3cb7a1e
Ian <ian.chen@ambernet.com.tw>
2021-03-17 11:56:41 +0800

    ++

--- cmds/releaseIOS.sh
+++ cmds/releaseIOS.sh
@@ -45,17 +45,39 @@ echo "<?xml version="1.0" encoding="UTF-8"?>
 </dict>
 </plist>" > ../../tempReleaseFolder/$1.plist
 
+
+
 #步驟4: 將下載頁拉下來，放進暫存資料夾
 sshpass -p "27ZrKw^h2e2rFnph" scp -P 22897 TW_RD@10.6.1.100:/home/img-server/bm-$2-iOS.html ../../tempReleaseFolder/
 
 #步驟5: 插入下載鏈結
+#步驟5: 插入下載鏈結
+
+if [ '$2' = "stg" ]
+then
+   sed -ie '1i\
+<H1><a href="itms-services://?action=download-manifest&amp;url=https://dl.dropboxusercontent.com/s/2loj8bmorcu0sow/BM_test_NEW.plist">'$1'</a></H1>
+' ../../tempReleaseFolder/bm-$2-iOS.html
+else
+    sed -ie '1i\
+<H1><a href="itms-services://?action=download-manifest&amp;url=https://dl.dropboxusercontent.com/s/rbd8eqmu1699m2e/BM_PRD_NEW.plist">'$1'</a></H1>
+' ../../tempReleaseFolder/bm-$2-iOS.html
+fi
+
+
 sed -ie '1i\
 <H1><a href="itms-services://?action=download-manifest&amp;url=https://dl.phl5b.org/iOS/bm_'$2'/'$1'.plist">'$1'</a></H1>
 ' ../../tempReleaseFolder/bm-$2-iOS.html
 
+
 #步驟6: 上傳ipa, plist
 sshpass -p "27ZrKw^h2e2rFnph" scp -P 22897 ../../tempReleaseFolder/$1.ipa TW_RD@10.6.1.100:/home/img-server/iOS/bm_$2/
 sshpass -p "27ZrKw^h2e2rFnph" scp -P 22897 ../../tempReleaseFolder/$1.plist TW_RD@10.6.1.100:/home/img-server/iOS/bm_$2/
-
+if [ '$2' = "stg" ]
+then
+cp ../../tempReleaseFolder/$1.ipa $HOME/Dropbox/BM_test_NEW.ipa
+else
+cp ../../tempReleaseFolder/$1.ipa $HOME/Dropbox/BM_PRD_NEW.ipa
+fi
 #步驟7: 上傳html
 sshpass -p "27ZrKw^h2e2rFnph" scp -P 22897 ../../tempReleaseFolder/bm-$2-iOS.html TW_RD@10.6.1.100:/home/img-server/
